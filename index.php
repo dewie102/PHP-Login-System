dae50e4dbb8024faf9cd333edbdae1af4c391a9b
Zackariah Romasz <zackariah.romasz@yahoo.com>
2018-05-28 10:19:16 -0700

    Added config; added login/reg pages; added footer

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+	// Allow the config
+	define('__CONFIG__', true);
+	// Requre the config
+	require_once "inc/config.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -16,37 +23,17 @@
 	<body>
 
 		<div class="uk-section uk-container">
-			<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
-
-				<form class="uk-form-stacked js-login">
-				    <div class="uk-margin">
-				        <label class="uk-form-label" for="form-stacked-text">Email</label>
-				        <div class="uk-form-controls">
-				            <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
-				        </div>
-				    </div>
-
-				    <div class="uk-margin">
-				        <label class="uk-form-label" for="form-stacked-text">Password</label>
-				        <div class="uk-form-controls">
-				            <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your Password">
-				        </div>
-				    </div>
-
-					<div class="uk-margin">
-						<button class="uk-button uk-button-default" type="submit">Login</button>
-					</div>
-				</form>
-
-			</div>
+			<?php 
+				echo "Hello World. Today is: ";
+				echo date("Y m d");
+			?>
+			<p>
+				<a href="php_login_course/login.php">Login</a>
+				<a href="php_login_course/register.php">Register</a>
+			</p>
 		</div>
 
-		<!-- jQuery is required? -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<?php require_once "inc/footer.php"; ?>
 
-		<!-- UIkit JS -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.3/js/uikit.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.3/js/uikit-icons.min.js"></script>
-		
 	</body>
 </html>
