8d534dbc60a3085e06a3651bb07542ee289f40e7
Zackariah Romasz <zackariah.romasz@yahoo.com>
2018-06-03 17:14:16 -0700

    Added login via ajax

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
-$(document).on("submit", "form.js-register", function(event) {
+$(document)
+.on("submit", "form.js-register", function(event) {
 	event.preventDefault();
 
 	var _form = $(this);
@@ -9,7 +10,7 @@ $(document).on("submit", "form.js-register", function(event) {
 		password: $("input[type='password']", _form).val()
 	};
 
-	if(dataObj.email.length < 8) {
+	if(dataObj.email.length < 6) {
 		_error
 			.text("Please enter a valid email address")
 			.show();
@@ -47,6 +48,60 @@ $(document).on("submit", "form.js-register", function(event) {
 	.always(function ajaxAlwaysDoThis(data) {
 		// Always do
 		console.log('Always');
+	})
+
+	return false;
+})
+
+// 
+.on("submit", "form.js-login", function(event) {
+	event.preventDefault();
+
+	var _form = $(this);
+	var _error = $(".js-error", _form);
+
+	var dataObj = {
+		email: $("input[type='email']", _form).val(),
+		password: $("input[type='password']", _form).val()
+	};
+
+	if(dataObj.email.length < 6) {
+		_error
+			.text("Please enter a valid email address")
+			.show();
+		return false;
+	} else if(dataObj.password.length < 11) {
+		_error
+			.text("Please enter a password that is at least 11 characters long.")
+			.show();
+		return false;
+	}
+
+	// Assuming the code gets this far, we can start the ajax process
+	_error.hide();
+
+	$.ajax({
+		type: 'POST',
+		url: '/php_login_course/ajax/login.php',
+		data: dataObj,
+		dataType: 'json',
+		async: true,
+	})
+	.done(function ajaxDone(data) {
+		// Whatever data is
+		if(data.redirect !== undefined) {
+			window.location = data.redirect;
+		} else if(data.error !== undefined) {
+			_error
+				.html(data.error)
+				.show();
+		}
+	})
+	.fail(function ajaxFailed(e) {
+		// This failed
+	})
+	.always(function ajaxAlwaysDoThis(data) {
+		// Always do
 	})
 
 	return false;
--- inc/config.php
+++ inc/config.php
@@ -4,6 +4,11 @@
 		exit("You do not have a config file");
 	}
 
+	// Session are always turned on
+	if(!isset($_SESSION)) {
+		session_start();
+	}
+
 	// Our config is below
 	// Allow errors
 	error_reporting(-1);
